30f408d7f1338adbe5ea7f82d9b4e9758a7d8655
Thanh Tung <nguyenhoangthanhtung1610@gmail.com>
2019-10-31 01:52:05 +0700

    update seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(StatusesTableSeeder::class);
-        $this->call(ProductsTableSeeder::class);
-        $this->call(BillsTableSeeder::class);
-        $this->call(OrdersTableSeeder::class);
+        // $this->call(ProductsTableSeeder::class);
+        // $this->call(BillsTableSeeder::class);
+        // $this->call(OrdersTableSeeder::class);
     }
 }
