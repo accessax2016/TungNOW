acda807aa5a2696b01133809729e0e9ce2547976
Thanh Tung <nguyenhoangthanhtung1610@gmail.com>
2019-12-20 00:21:56 +0700

    update

--- public/js/app.js
+++ public/js/app.js
@@ -11670,7 +11670,7 @@ __webpack_require__.r(__webpack_exports__);
       var oauth = {
         grant_type: "password",
         client_id: 2,
-        client_secret: '9MXTHSNwPATpuRwBrFjyiehNnCzuJ3dkTJlvAdze',
+        client_secret: '1acS06uP355NjqWqA3KfEI6wPC93z318UIVnlv4M',
         // client_secret: "TgHj8DAJWDplX4ErK1cP0ASonDk6xlOcxboEbX3w",
         username: this.email,
         password: this.password
